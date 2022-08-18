b874b4c36792ac809a6ebbdaf2caef0ba8f23375
Muhammad Umer Saeed <umersaeed3d@gmail.com>
2022-08-18 11:52:29 +0500

    Overriden login functonalities

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
