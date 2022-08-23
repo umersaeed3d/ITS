9bbb0bac837eb14b316d6f074134605cf11637a9
Muhammad Umer Saeed <umersaeed3d@gmail.com>
2022-08-23 13:00:11 +0500

    New modifications

--- resources/views/lab/index.blade.php
+++ resources/views/lab/index.blade.php
@@ -12,7 +12,7 @@
                         <h2>Add Lab</h2>
                     </div>
                     <div class="modal-body">
-                        <form action="/labs/" method="POST" enctype="multipart/form-data">
+                        <form action="/labs" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="addInvBody">
 
